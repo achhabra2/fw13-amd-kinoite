df742974837249287da9537ddface98b0a4a9176
Aman <17457975+achhabra2@users.noreply.github.com>
2023-11-11 14:09:29 -0800

    Fixes
    Fix boot menu
    Edit recipe

---
+++ config/scripts/rpm_ostree_install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Your code goes here.
+echo 'Installing Packages'
+
+rpm-ostree install brave-browser 1password
+rpm-ostree install https://github.com/Umio-Yasuno/amdgpu_top/releases/download/v0.3.1/amdgpu_top-0.3.1-1.x86_64.rpm
