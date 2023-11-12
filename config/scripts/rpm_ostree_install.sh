0d2c2d42eb947dabc8e5f9d3a1c1673c4dd3342d
Aman <17457975+achhabra2@users.noreply.github.com>
2023-11-11 17:34:49 -0800

    Shift Packages Around
    Hopefully this will work

--- config/scripts/rpm_ostree_install.sh
+++ config/scripts/rpm_ostree_install.sh
@@ -8,8 +8,4 @@ set -oue pipefail
 # Your code goes here.
 echo 'Installing Packages'
 
-cp /tmp/config/files/usr/etc/yum.repos.d/1password.repo /etc/yum.repos.d/1password.repo
-cp /tmp/config/files/usr/etc/yum.repos.d/brave.repo /etc/yum.repos.d/brave.repo
-
-rpm-ostree install brave-browser 1password
 rpm-ostree install https://github.com/Umio-Yasuno/amdgpu_top/releases/download/v0.3.1/amdgpu_top-0.3.1-1.x86_64.rpm
