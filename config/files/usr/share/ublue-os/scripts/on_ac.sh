18e0ddb1d6233b127bae86e4719d4bf35380a3b6
Aman <17457975+achhabra2@users.noreply.github.com>
2023-11-28 11:49:34 -0800

    Update on_bat / on_ac rules
    Now that PPD works, we can readjust the settings

--- config/files/usr/share/ublue-os/scripts/on_ac.sh
+++ config/files/usr/share/ublue-os/scripts/on_ac.sh
@@ -5,5 +5,9 @@ echo '500' > '/proc/sys/vm/dirty_writeback_centisecs';
 
 # Change AMD Paste EPP energy preference
 # Available profiles: performance, balance_performance, balance_power, power
-echo 'balance_performance' | tee /sys/devices/system/cpu/cpufreq/policy*/energy_performance_preference;
+# echo 'balance_performance' | tee /sys/devices/system/cpu/cpufreq/policy*/energy_performance_preference;
 
+echo 'performance' | tee /sys/module/pcie_aspm/parameters/policy
+
+# Use patched PPD, automatically sets EPP to balance_performance
+powerprofilesctl set balanced
--- config/files/usr/share/ublue-os/scripts/on_battery.sh
+++ config/files/usr/share/ublue-os/scripts/on_battery.sh
@@ -3,7 +3,11 @@
 # Change Dirty Writeback Centisecs according to TLP / Powertop
 echo '1500' > '/proc/sys/vm/dirty_writeback_centisecs';
 
+# Set PPD on Unplug, user can always manually override
+powerprofilesctl set power-saver
+
 # Change AMD Paste EPP energy preference
 # Available profiles: performance, balance_performance, balance_power, power
-echo 'balance_power' | tee /sys/devices/system/cpu/cpufreq/policy*/energy_performance_preference;
+# echo 'balance_power' | tee /sys/devices/system/cpu/cpufreq/policy*/energy_performance_preference;
 
+echo 'powersupersave' | tee /sys/module/pcie_aspm/parameters/policy
