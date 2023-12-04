9846815349808848804730c9a1c6ae8911e2fe51
Aman <17457975+achhabra2@users.noreply.github.com>
2023-12-03 18:19:23 -0800

    Create mesa_git.sh
    Add script to install mesa-git from copr

---
+++ config/scripts/mesa_git.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Your code goes here.
+echo 'Setting Up Mesa Drivers from Git'
+
+rpm-ostree remove mesa-va-drivers-freeworld
+
+rpm-ostree override replace --experimental \
+    --from repo=copr:copr.fedorainfracloud.org:xxmitsu:mesa-git \
+    mesa-filesystem \
+    mesa-libxatracker \
+    mesa-libglapi \
+    mesa-dri-drivers \
+    mesa-libgbm \
+    mesa-libEGL \
+    mesa-vulkan-drivers \
+    mesa-libGL
+
+rpm-ostree install mesa-va-drivers
+
