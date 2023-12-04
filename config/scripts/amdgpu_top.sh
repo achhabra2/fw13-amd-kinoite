9f43a92ad9f37dff4ae596db77147b511927630d
Aman <17457975+achhabra2@users.noreply.github.com>
2023-12-03 18:18:46 -0800

    Rename script
    Rename to more appropriate amdgpu_top.sh

--- config/scripts/amdgpu_top.sh
+++ config/scripts/amdgpu_top.sh
@@ -6,6 +6,6 @@
 set -oue pipefail
 
 # Your code goes here.
-echo 'Installing Packages'
+echo 'Installing AMDGPU_TOP'
 
 rpm-ostree install https://github.com/Umio-Yasuno/amdgpu_top/releases/download/v0.3.1/amdgpu_top-0.3.1-1.x86_64.rpm
